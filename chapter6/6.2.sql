534ed72b6831a6236a864706dbbb1cbd850a8438
odnaks <xvluk@ya.ru>
2019-04-17 11:32:46 +0300

    add chapter 6

---
+++ chapter6/6.2.sql
@@ -0,0 +1,5 @@
+SELECT fname, lname
+FROM individual
+UNION
+SELECT fname, lname
+FROM employee;
---
+++ chapter6/6.3.sql
@@ -0,0 +1,6 @@
+SELECT fname, lname
+FROM individual
+UNION
+SELECT fname, lname
+FROM employee
+ORDER BY lname;
